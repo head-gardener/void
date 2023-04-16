63e05de79e4bd29dc8161cd447d61c0822bdc5d3
head-gardener <trashbin2019np@gmail.com>
2023-04-16 07:56:25 +0300

    fixed high cpu load

--- voidgui/src/voidgui.c
+++ voidgui/src/voidgui.c
@@ -1,5 +1,6 @@
 #include "draw_queue.h"
 #include "shapes.h"
+#include <stdbool.h>
 #define GLEW_STATIC
 
 #include "macros.h"
@@ -12,6 +13,8 @@
 #define VOID_SANER
 
 struct void_window *void_gui_init(int time) {
+  setbuf(stdout, NULL);
+
   SDL_Init(SDL_INIT_EVERYTHING);
 
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
@@ -38,63 +41,72 @@ struct void_window *void_gui_init(int time) {
   return v_window;
 }
 
-int void_gui_exec(struct void_window *window) {
-  SDL_Event window_event;
-  while (SDL_PollEvent(&window_event)) {
-    Uint32 time = SDL_GetTicks();
-
-    clear(&window->painter);
-
-    switch (window_event.type) {
-    case SDL_QUIT:
-      return 0;
-    case SDL_MOUSEBUTTONUP: {
-      struct point attribs = {window_event.motion.x, window_event.motion.y};
-      catch (&window->painter, &window->click_sink, &window->text_input_sink,
-             &window->draw_queue, &window->store, &window->click_sink,
-             &attribs);
-      break;
-    }
-    case SDL_KEYDOWN:
-      switch (window_event.key.keysym.scancode) {
-      case (SDL_SCANCODE_RETURN): {
-        struct text_event event = {0, TEXT_EVENT_COMMIT};
-        catch (&window->painter, &window->click_sink, &window->text_input_sink,
-               &window->draw_queue, &window->store, &window->text_input_sink,
-               &event);
-        break;
-      }
-      case (SDL_SCANCODE_ESCAPE): {
-        struct text_event event = {0, TEXT_EVENT_CANCEL};
-        catch (&window->painter, &window->click_sink, &window->text_input_sink,
-               &window->draw_queue, &window->store, &window->text_input_sink,
-               &event);
-        break;
-      }
-      case (SDL_SCANCODE_LEFT): {
-        struct text_event event = {0, TEXT_EVENT_CURSOR_LEFT};
-        catch (&window->painter, &window->click_sink, &window->text_input_sink,
-               &window->draw_queue, &window->store, &window->text_input_sink,
-               &event);
-        break;
-      }
-      case (SDL_SCANCODE_RIGHT): {
-        struct text_event event = {0, TEXT_EVENT_CURSOR_RIGHT};
-        catch (&window->painter, &window->click_sink, &window->text_input_sink,
-               &window->draw_queue, &window->store, &window->text_input_sink,
-               &event);
-        break;
-      }
-      default:;
-      }
-      break;
-    case SDL_TEXTINPUT: {
-      struct text_event event = {window_event.text.text, TEXT_EVENT_INPUT};
+int handle_event(struct void_window *window, SDL_Event window_event) {
+  switch (window_event.type) {
+  case SDL_QUIT:
+    return 0;
+  case SDL_MOUSEBUTTONUP: {
+    struct point attribs = {window_event.motion.x, window_event.motion.y};
+    catch (&window->painter, &window->click_sink, &window->text_input_sink,
+           &window->draw_queue, &window->store, &window->click_sink, &attribs);
+    break;
+  }
+  case SDL_KEYDOWN:
+    switch (window_event.key.keysym.scancode) {
+    case (SDL_SCANCODE_RETURN): {
+      struct text_event event = {0, TEXT_EVENT_COMMIT};
       catch (&window->painter, &window->click_sink, &window->text_input_sink,
              &window->draw_queue, &window->store, &window->text_input_sink,
              &event);
       break;
     }
+    case (SDL_SCANCODE_ESCAPE): {
+      struct text_event event = {0, TEXT_EVENT_CANCEL};
+      catch (&window->painter, &window->click_sink, &window->text_input_sink,
+             &window->draw_queue, &window->store, &window->text_input_sink,
+             &event);
+      break;
+    }
+    case (SDL_SCANCODE_LEFT): {
+      struct text_event event = {0, TEXT_EVENT_CURSOR_LEFT};
+      catch (&window->painter, &window->click_sink, &window->text_input_sink,
+             &window->draw_queue, &window->store, &window->text_input_sink,
+             &event);
+      break;
+    }
+    case (SDL_SCANCODE_RIGHT): {
+      struct text_event event = {0, TEXT_EVENT_CURSOR_RIGHT};
+      catch (&window->painter, &window->click_sink, &window->text_input_sink,
+             &window->draw_queue, &window->store, &window->text_input_sink,
+             &event);
+      break;
+    }
+    default:;
+    }
+    break;
+  case SDL_TEXTINPUT: {
+    struct text_event event = {window_event.text.text, TEXT_EVENT_INPUT};
+    catch (&window->painter, &window->click_sink, &window->text_input_sink,
+           &window->draw_queue, &window->store, &window->text_input_sink,
+           &event);
+    break;
+  }
+  }
+
+  return 1;
+}
+
+int void_gui_exec(struct void_window *window) {
+  SDL_Event window_event;
+  while (true) {
+    Uint32 time = SDL_GetTicks();
+
+    clear(&window->painter);
+
+    while (SDL_PollEvent(&window_event)) {
+      int code = handle_event(window, window_event);
+      if (code != 1)
+        return code;
     }
 
     foreach_node(window->draw_queue.head, draw_node(&window->painter, node));
