549ad02964d0b534370567d4b802e45382b2e2db
head-gardener <trashbin2019np@gmail.com>
2023-05-16 20:06:55 +0300

    rework: integrated StateT

--- void/app/Database/Interface.hs
+++ void/app/Database/Interface.hs
@@ -1,16 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Database.Interface (connection, pull, push) where
+module Database.Interface (connectInfo, connection, pull, push) where
 
 import Control.Exception (SomeException, try)
 import Control.Exception.Base (Exception)
+import Control.Monad.Trans.Except
+import Data.Functor ((<&>))
 import Database.PostgreSQL.Simple as PG
 import Debug.Trace (trace)
 import Entry
 import GUI (Damage (..))
 
-localPG :: ConnectInfo
-localPG =
+connectInfo :: ConnectInfo
+connectInfo =
   defaultConnectInfo
     { connectHost = "127.0.0.1"
     , connectDatabase = "postgres"
@@ -18,17 +20,17 @@ localPG =
     , connectPassword = "postgres"
     }
 
-connection :: IO (Either SomeException Connection)
-connection = try $ PG.connect localPG
+connection :: ExceptT SomeException IO Connection
+connection = ExceptT $ try $ PG.connect connectInfo
 
 pull :: Connection -> IO [Entry]
 pull conn = PG.query_ conn "select * from clients"
 
-push :: Connection -> [Damage] -> IO ()
+push :: Connection -> [Damage] -> IO Int
 push c xs = do
-  mapM f xs >>= (\x -> putStrLn ("changed " ++ show x ++ " rows")) . sum
+  mapM update xs <&> fromIntegral . sum
  where
-  f (Update uuid n from _) =
+  update (Update uuid n from _) =
     case n of
       0 ->
         PG.execute c "update clients set name = ? where id = ?" (from, uuid)
--- void/app/Main.hs
+++ void/app/Main.hs
@@ -1,10 +1,21 @@
 module Main where
 
+import Control.Exception
+import Control.Monad (void, (>=>))
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Except (Except, ExceptT (..), runExceptT)
+import Control.Monad.Trans.State.Lazy
 import Database.Interface as DB
+import Database.PostgreSQL.Simple (ConnectInfo, Connection)
 import Entry
 import Foreign.C.Types
 import qualified GUI as G
 import Text.Printf
+
+initialState :: G.VoidInstance -> ConnectInfo -> WindowState
+initialState w i = (w, DB.connection, False, i)
+
+type WindowState = (G.VoidInstance, ExceptT SomeException IO Connection, Bool, ConnectInfo)
 
 main :: IO ()
 main =
@@ -12,19 +23,37 @@ main =
     fill w
     exec w
  where
-  pull :: G.VoidInstance -> IO ()
-  pull w = do
-    conn <- DB.connection
-    case conn of
-      Left _ -> G.putStatus w "Connection error!" -- ++ show e
-      Right conn -> DB.pull conn >>= (G.drop w `seq` G.push w)
+  tryWithSyncedConn :: (Connection -> IO ()) -> StateT WindowState IO ()
+  tryWithSyncedConn f = do
+    (w, _, s, _) <- get
+    if s
+      then tryWithConnection f
+      else liftIO $ G.putStatus w "Not synced!"
 
-  push :: G.VoidInstance -> IO ()
-  push w = do
-    conn <- DB.connection
-    case conn of
-      Left _ -> G.putStatus w "Connection error!" -- ++ show e
-      Right conn -> G.pull w >>= DB.push conn
+  tryWithConnection :: (Connection -> IO ()) -> StateT WindowState IO ()
+  tryWithConnection f = do
+    (w, c, s, i) <- get
+    liftIO (runExceptT c >>= handleConnection (w, c, s, i) f) >>= put
+   where
+    handleConnection :: WindowState -> (Connection -> IO ()) -> Either SomeException Connection -> IO WindowState
+    handleConnection (w, c, _, i) f (Left e) = G.putStatus w "Connection error!" >> return (initialState w i)
+    handleConnection (w, _, s, i) f (Right c) = liftIO (f c) >> return (w, ExceptT $ return $ Right c, True, i)
+
+  pull :: StateT WindowState IO ()
+  pull = do
+    (w, c, _, _) <- get
+    tryWithConnection
+      ( DB.pull
+          >=> (\es -> G.drop w `seq` G.putStatus w "Pull successful" >> G.push w es)
+      )
+
+  push :: StateT WindowState IO ()
+  push = do
+    (w, c, _, _) <- get
+    tryWithSyncedConn (\c -> G.pull w >>= DB.push c >>= prettyPrint w)
+   where
+    prettyPrint :: G.VoidInstance -> Int -> IO ()
+    prettyPrint w n = G.putStatus w $ "Changes pushed: " ++ show n
 
   fill :: G.VoidInstance -> IO ()
   fill w =
@@ -35,11 +64,13 @@ main =
       ]
 
   exec :: G.VoidInstance -> IO ()
-  exec window = do_exec window 0
+  exec w = evalStateT do_exec $ initialState w DB.connectInfo
 
-  do_exec :: G.VoidInstance -> CInt -> IO ()
-  do_exec w 0 = do_exec w $ G.wait w
-  do_exec w 3 = push w >> do_exec w (G.wait w)
-  do_exec w 2 = pull w >>= \x -> x `seq` do_exec w (G.wait w)
-  do_exec w 1 = return ()
-  do_exec w code = putStrLn ("Unexpected code " ++ show code) >> do_exec w (G.wait w)
+  do_exec :: StateT WindowState IO ()
+  do_exec = do
+    (w, _, _, _) <- get
+    case G.wait w of
+      1 -> return ()
+      2 -> pull >> do_exec
+      3 -> push >> do_exec
+      _ -> do_exec
