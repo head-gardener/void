61546ed1bc76ca7aee3274b23563b78f5ae191f1
head-gardener <trashbin2019np@gmail.com>
2023-05-18 20:31:37 +0300

    feat: search a dataset

--- voidgui/voidgui/src/logic/dataset.rs
+++ voidgui/voidgui/src/logic/dataset.rs
@@ -1,10 +1,16 @@
+type Search = Option<(String, usize)>;
+
 pub struct Dataset {
   records: Vec<Box<(String, String)>>,
+  search: Search,
 }
 
 impl Dataset {
   pub fn new() -> Self {
-    Self { records: vec![] }
+    Self {
+      records: vec![],
+      search: None,
+    }
   }
 
   pub fn clear(&mut self) {
@@ -25,5 +31,132 @@ impl Dataset {
 
   pub fn get(&self, n: usize) -> &str {
     &self.records[n].0
+  }
+
+  pub fn new_search(&mut self, value: &str) {
+    self.search = Some((value.to_string().to_lowercase(), 0));
+  }
+
+  fn clear_search(&mut self) {
+    self.search = None;
+  }
+
+  fn find_prev(&mut self) -> Option<usize> {
+    self.search.as_mut().and_then(|(s, i)| {
+      self
+        .records
+        .iter()
+        .rev()
+        .chain(self.records.iter().rev())
+        .skip(self.records.len() - *i + 1)
+        .position(|b| b.1.contains(s.as_str()))
+        .map(|r| {
+          if r >= *i {
+            *i += self.records.len() + 1
+          }
+          *i -= r + 1;
+          *i - 1
+        })
+    })
+  }
+
+  fn find_next(&mut self) -> Option<usize> {
+    self.search.as_mut().and_then(|(s, i)| {
+      self
+        .records
+        .iter()
+        .chain(self.records.iter())
+        .skip(*i)
+        .position(|b| b.1.contains(s.as_str()))
+        .map(|r| {
+          *i += r + 1;
+          if *i > self.records.len() {
+            *i -= self.records.len()
+          }
+          *i - 1
+        })
+    })
+  }
+}
+
+#[cfg(test)]
+mod test_dataset {
+  use super::*;
+
+  #[test]
+  fn basics() {
+    let mut d = Dataset::new();
+    d.push("hi");
+    assert_eq!(d.get(0), "hi");
+    d.set(0, "hello");
+    assert_eq!(d.get(0), "hello");
+    d.clear();
+    d.push("yup");
+    assert_eq!(d.get(0), "yup");
+  }
+
+  #[test]
+  fn search_basics() {
+    let mut d = Dataset::new();
+
+    // no search - no finds
+    assert_eq!(d.find_next(), None);
+    assert_eq!(d.find_prev(), None);
+
+    ["hello"].into_iter().for_each(|x| d.push(x));
+    d.new_search("He");
+
+    // next works
+    assert_eq!(d.find_next(), Some(0));
+    d.clear_search();
+
+    // no search - no finds
+    assert_eq!(d.find_next(), None);
+    assert_eq!(d.find_prev(), None);
+  }
+
+  #[test]
+  fn search_good() {
+    let mut d = Dataset::new();
+
+    ["hello", "hi", "Hey", "ahead"]
+      .into_iter()
+      .for_each(|x| d.push(x));
+    d.new_search("He");
+
+    // next works
+    assert_eq!(d.find_next(), Some(0));
+    assert_eq!(d.find_next(), Some(2));
+    assert_eq!(d.find_next(), Some(3));
+
+    // prev works
+    assert_eq!(d.find_prev(), Some(2));
+    assert_eq!(d.find_next(), Some(3));
+  }
+
+  #[test]
+  fn search_good_wrap() {
+    let mut d = Dataset::new();
+
+    ["hello", "hi", "Hey", "ahead"]
+      .into_iter()
+      .for_each(|x| d.push(x));
+    d.new_search("He");
+
+    assert_eq!(d.find_prev(), Some(3), "find_prev should wrap");
+    assert_eq!(d.find_next(), Some(0), "find_next should wrap");
+  }
+
+  #[test]
+  fn search_bad() {
+    let mut d = Dataset::new();
+
+    ["hello", "hi", "Hey", "ahead"]
+      .into_iter()
+      .for_each(|x| d.push(x));
+    d.new_search("miss");
+
+    assert_eq!(d.find_prev(), None);
+    assert_eq!(d.find_prev(), None);
   }
 }
